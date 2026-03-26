27fb9b9efe22baf658cc5e9e9889a4d75c5ea934
prasad <aprasadreddy9@gmail.com>
2026-03-26 16:46:36 +0530

    added authenticatin module

---
+++ backend/modules/auth/auth.controller.js
@@ -0,0 +1,46 @@
+import { validateResult } from "express-validator";
+import * as authService from "./auth.service.js";
+
+const signup = async(req,res, next) =>{
+
+    try{
+        const errors = validateResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({success:false, message:errors.arrya()[0].msg});
+
+        }
+
+        const data = await authService.signup(req.body);
+        res.status(201).json({success:true, message:"Account created", data});
+
+    }catch (error){
+        next(error);
+    }
+};
+
+
+const login = async (req, res, next) => {
+
+    try{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            return res.status(400).json({success:false, message:errors.arrays()[0].msg});
+
+        }
+        const data = await authService.login(req.body);
+        res.status(200).json({success:true, message:"Login successful", data});
+    } catch(error){
+        next(error);
+    }
+};
+
+const getMe = async(req,res, next) => {
+    try{
+        const user = await authService.getMe(req.user._id);
+        res.status(200).json({success:true, data:{ user }});
+    }catch(error){
+        next(error);
+    }
+};
+
+export { signup, login, getMe };
---
+++ backend/modules/auth/auth.routes.js
@@ -0,0 +1,29 @@
+import express from "express";
+import { body } from "express-validator";
+import { signup, login, getMe} from "./auth.controller.js";
+import { protect } from "../../middleware/auth.middleware.js";
+
+
+const router = express.Router();
+
+router.post(
+    "/signup",
+    [
+        body("name").trim().notEmpty().withMessage("Name is required"),
+        body("email").isEmail().withMessage("Enter a valid emial"),
+        body("password").isLength({min: 6}),withMessage("Password must be at least 6 characters"),
+    ],
+    signup
+);
+
+router.post("/login",
+    [
+        body("email").isEmail().withMessage("Enter a valid email"),
+        body("password").notEmpty().withMessage("Password is required"),
+    ],
+    login
+);
+
+router.get("/me", protect, getMe);
+
+export default router;
