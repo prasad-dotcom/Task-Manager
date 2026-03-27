c68f6f43225cd672805c0de73076bfd603b645c7
prasad <aprasadreddy9@gmail.com>
2026-03-27 12:03:56 +0530

    Updated README

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,16 @@
 import mongoose from "mongoose";
 
-
 const connectDB = async () => {
     console.log("[DEBUG] Attempting to connect to MongoDB URI:", process.env.MONGODB_URI);
     try {
         await mongoose.connect(process.env.MONGODB_URI);
         console.log("[DEBUG] MongoDb connected");
+        return true;
     } catch (error) {
-        console.log("[DEBUG] DB Error:", error.message);
-        process.exit(1);
+        console.error("[DEBUG] DB Error:", error.message);
+        // Don't exit - just throw error to be handled by caller
+        throw error;
     }
 };
-
 
 export default connectDB;
